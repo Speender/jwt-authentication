19d432b8e360b6f36c237e81e732fd8dd2dba4ce
Theo <ramiepondar11@gmail.com>
2025-11-17 11:18:07 +0800

    chore: jwt_auth & login

---
+++ flask_file_upload_mvc/controllers/jwt_auth.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, request, jsonify
+import logging
+import datetime
+from config import Config
+import jwt
+
+auth_bp = Blueprint('auth_bp', __name__)
+
+#Post - Login Endpoint
+#user and password
+@auth_bp.route('/login', methods=['POST'])
+
+def login():
+    credentials = request.get_json()
+
+    if not credentials or not credentials.get('username') or not credentials.get('password'):
+        logging.warning(f"Empty fields")
+        return jsonify({'message': 'Username and password are required'}), 400
+
+    username = credentials['username']
+    password = credentials['password']
+
+    if username == 'user' and password == 'pass':
+        token = jwt.encode({
+            'username': username,
+            'exp': datetime.datetime.now() + datetime.timedelta(minutes=10)
+        }, Config.SECRET_KEY, algorithm = 'HS256')
+        logging.info(f'[LOGIN SUCCESS] Username: {username}')
+        return jsonify({f'message': 'Login Successful', 'username': username,'token': token}), 200
+
+    logging.warning(f'[LOGIN FAILED] Username: {username}')
+    return jsonify({'message': 'Invalid credentials'}), 401
+    
